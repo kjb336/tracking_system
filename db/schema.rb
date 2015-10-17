2b3b24eb3c147aaad56f1f6a94afc18cd40ded48
kjb336 <kjb336@gmail.com>
2015-10-17 16:48:04 -0400

    Created the project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017022924) do
+ActiveRecord::Schema.define(version: 20151017203346) do
 
   create_table "members", force: :cascade do |t|
     t.string   "fname"
     t.string   "lname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.text     "title"
+    t.date     "date"
+    t.text     "description"
+    t.text     "project_manager"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "targets", force: :cascade do |t|
